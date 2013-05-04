eb55c86f4501158dab1734af448eece57a6da2a4
OmeGak <omegak@gmail.com>
2013-05-04 13:35:10 +0200

    System files updated

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -3,8 +3,19 @@
 #   `brew install coreutils`
 if $(gls &>/dev/null)
 then
-  alias ls="gls -F --color"
-  alias l="gls -lAh --color"
-  alias ll="gls -l --color"
-  alias la='gls -A --color'
+    LS="gls"
+else
+    LS="ls"
 fi
+
+# places
+alias ..="cd .."
+alias \!="cd $HOME/Dropbox/#Workshop"
+alias code="cd $HOME/Code"
+
+# ls
+alias ls="$LS -F --color"
+alias ll="$LS -lh --group-directories-first --color"
+alias lla="$LS -lhA --group-directories-first --color"
+
+unset LS
