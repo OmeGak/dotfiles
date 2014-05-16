0594432715d33994646ea7224a530a79deb8d366
OmeGak <omegak@gmail.com>
2014-05-16 08:51:40 +0200

    More Linux-friendly scripts

--- python/install.sh
+++ python/install.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-brew install python 2> /tmp/brew-python
+if [[ "$(uname -s)" == "Darwin" ]]; then
+  brew install python 2> /tmp/brew-python
+  pprint ok "Python freshly brewed"
+elif [[ "$(uname -s)" == "Linux" ]]; then
+  sudo apt-get install python > /tmp/apt-get-python
+  pprint ok "Python freshly installed"
+else
+  pprint error "Unrecognized OS"
+  exit 1
+fi
 
-pprint ok "Python freshly brewed"
 exit 0
