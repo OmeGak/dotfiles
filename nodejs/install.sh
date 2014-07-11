b287089bfc42cf81873892f9d2e549ef1c3e6d13
OmeGak <omegak@gmail.com>
2014-07-11 16:54:10 +0200

    NPM global requires now sudo and ST finds jshint

--- nodejs/install.sh
+++ nodejs/install.sh
@@ -10,12 +10,13 @@ elif [[ "$(uname -s)" == "Linux" ]]; then
   sudo apt-get update > /tmp/apt-get-update.dot.log 2>&1
   sudo apt-get install nodejs > /tmp/apt-get-nodejs.dot.log 2>&1
   pprint ok "NodeJS freshly installed"
+  sudo="sudo"
 else
   pprint error "Unrecognized OS"
   exit 0
 fi
 
-npm install -g jshint > /tmp/npm-install.dot.log 2>&1
+$sudo npm install -g jshint > /tmp/npm-install.dot.log 2>&1
 if [[ $? == 0 ]]; then
   pprint error "Failed to install NodeJS global packages"
   exit 1
--- nodejs/path.zsh
+++
@@ -1,4 +0,0 @@
-NPM_PACKAGES="$HOME/.npm"
-
-export NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
-export PATH="$NPM_PACKAGES/bin:$PATH"
