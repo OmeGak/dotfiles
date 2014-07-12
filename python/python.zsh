91e70b143eacac3c1a4d20412bea1c9e0145c977
OmeGak <omegak@gmail.com>
2014-07-12 14:29:57 +0200

    Fixed Python install.sh

--- python/python.zsh
+++ python/python.zsh
@@ -3,13 +3,3 @@ export PIP_REQUIRE_VIRTUALENV=true
 
 # cache pip-installed packages to avoid re-downloading
 export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
-
-# Install global packages
-syspip() {
-  if [[ "$(uname -s)" == "Linux" ]]; then
-    sudo="sudo"
-  else
-    sudo=""
-  fi
-  PIP_REQUIRE_VIRTUALENV="" $sudo pip "$@"
-}
