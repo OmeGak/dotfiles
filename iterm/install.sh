072defec0cc99231725bc9bfd054682c659a076e
OmeGak <omegak@gmail.com>
2014-07-22 16:06:52 +0200

    Improve iterm install script
    * Ensure the script is not run from iterm
    * Ensure iterm doesn't load cached preferences

--- iterm/install.sh
+++ iterm/install.sh
@@ -5,15 +5,22 @@ if [[ "$(uname -s)" != "Darwin" ]]; then
   exit 0
 fi
 
+if [[ $TERM_PROGRAM = 'iTerm.app' ]]; then
+  pprint warning "Step skipped: you can't run this script from iTerm"
+  exit 0
+fi
+
+# Clean prefs cache
+killall cfprefsd
+
 plist_name="com.googlecode.iterm2.plist"
 plist_source="$ZSH/iterm/$plist_name"
 plist_target="$HOME/Library/Preferences/$plist_name"
 
 mv "$plist_target" "$plist_target.backup"
-pprint ok "Backed up installed preferences"
+pprint ok "Backed up old preferences"
 
-pprint warning "Make sure iTerm2 is not running for this step to work"
 cp "$plist_source" "$plist_target"
-pprint ok "Your preferences are now imported"
+pprint ok "Your preferences has been imported"
 
 exit 0
