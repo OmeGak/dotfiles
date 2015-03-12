0ce064918d1fe0cb4d42c40fdf7d282db2d577be
Alejandro Avilés <omegak@gmail.com>
2015-03-12 02:06:42 +0100

    Install PackageControll with ST

--- sublimetext/install.sh
+++ sublimetext/install.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
 if [[ "$(uname -s)" == "Darwin" ]]; then
-  sublime_dir="$HOME/Library/Application Support/Sublime Text 3/Packages"
+  sublime_dir="$HOME/Library/Application Support/Sublime Text 3"
 elif [[ "$(uname -s)" == "Linux" ]]; then
-  sublime_dir="$HOME/.config/sublime-text-3/Packages"
+  sublime_dir="$HOME/.config/sublime-text-3"
 else
   print error "Unrecognized OS"
   exit 0
 fi
 
-# Link ST settings
-rm -rf "$sublime_dir/Backup_User"
-mv "$sublime_dir/User" "$sublime_dir/Backup_User"
-pprint ok "Backed up installed preferences"
+sublime_pkgcontrol="$sublime_dir/Installed Packages/Package Control.sublime-package"
+sublime_pkgs_dir="$sublime_dir/Packages"
 
-ln -s "$ZSH/sublimetext/User" "$sublime_dir"
+# Install Package Control
+if [[ ! -e "$sublime_pkgcontrol" ]]; then
+  server_url="https://packagecontrol.io/Package%20Control.sublime-package"
+  curl -fsSL -o "$sublime_pkgcontrol" "$server_url"
+  pprint ok "Package Control installed"
+fi
+
+# Back up ST settings
+if [[ ! -d "$sublime_pkgs_dir/User.bak" ]]; then
+  mv "$sublime_pkgs_dir/User" "$sublime_pkgs_dir/User.bak"
+  pprint ok "Backed up installed preferences"
+fi
+
+# Link ST settings
+rm -rf "$sublime_pkgs_dir/User"
+ln -snf "$ZSH/sublimetext/User" "$sublime_pkgs_dir/User"
 pprint ok "Your preferences are now linked"
 
 pprint ok "SublimeText is ready to hack!"
