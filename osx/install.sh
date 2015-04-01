b63f2ba85ac32fcb9a70a27700cef949252f63c3
Alejandro Avilés <omegak@gmail.com>
2015-04-01 02:37:14 +0200

    osx: add instructions to enable NTFS write

--- osx/install.sh
+++ osx/install.sh
@@ -13,5 +13,14 @@ pprint ok "US-ES-Keyboard installed for user"
 
 # Install NTFS support
 brew install ntfs-3g > /tmp/brew-ntfs-3g.dot.log 2>&1
-pprint ok "NTFS support added to Mac OS"
+if [[ ! -L "/sbin/mount_ntfs" ]]; then
+  pprint warning "NTFS-3g is ready but you still need to replace the automounter manually"
+  pprint warning "This is a DANGEROUS operation, so proceed with care:"
+  pprint warning "  $ brew info ntfs-3g"
+  pprint warning "  $ sudo mv /sbin/mount_ntfs /sbin/mount_ntfs.orig"
+  pprint warning "  $ sudo ln -s <path_to_ntfs_version>/sbin/mount_ntfs /sbin/mount_ntfs"
+  exit 0
+fi
+
+pprint ok "Write to NTFS is enabled"
 exit 0
