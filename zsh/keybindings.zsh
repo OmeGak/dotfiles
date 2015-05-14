3c4eeeacc3d3c57c7452b6dd3d37f12b20a7a83f
Alejandro Avilés <omegak@gmail.com>
2015-05-14 02:33:38 +0200

    zsh: keybindings for handling alt-left/right

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -23,17 +23,19 @@ fi
 # -----------------------------------------------------------------------------
 
 # Bind the keys that zkbd set up to some widgets
-[[ -n "${key[Home]}" ]]    && bindkey "${key[Home]}"    beginning-of-line
-[[ -n "${key[End]}" ]]     && bindkey "${key[End]}"     end-of-line
-[[ -n "${key[Delete]}" ]]  && bindkey "${key[Delete]}"  delete-char
-[[ -n "${key[Up]}" ]]      && bindkey "${key[Up]}"      up-line-or-search
-[[ -n "${key[Down]}" ]]    && bindkey "${key[Down]}"    down-line-or-search
-[[ -n "${key[Left]}" ]]    && bindkey "${key[Left]}"    backward-char
-[[ -n "${key[Right]}" ]]   && bindkey "${key[Right]}"   forward-char
-[[ -n "${key[C-Up]}" ]]    && bindkey "${key[C-Up]}"    history-beginning-search-backward
-[[ -n "${key[C-Down]}" ]]  && bindkey "${key[C-Down]}"  history-beginning-search-forward
-[[ -n "${key[C-Left]}" ]]  && bindkey "${key[C-Left]}"  backward-word
-[[ -n "${key[C-Right]}" ]] && bindkey "${key[C-Right]}" forward-word
+[[ -n "${key[Home]}" ]]      && bindkey "${key[Home]}"      beginning-of-line
+[[ -n "${key[End]}" ]]       && bindkey "${key[End]}"       end-of-line
+[[ -n "${key[Delete]}" ]]    && bindkey "${key[Delete]}"    delete-char
+[[ -n "${key[Up]}" ]]        && bindkey "${key[Up]}"        up-line-or-search
+[[ -n "${key[Down]}" ]]      && bindkey "${key[Down]}"      down-line-or-search
+[[ -n "${key[Left]}" ]]      && bindkey "${key[Left]}"      backward-char
+[[ -n "${key[Right]}" ]]     && bindkey "${key[Right]}"     forward-char
+[[ -n "${key[C-Up]}" ]]      && bindkey "${key[C-Up]}"      history-beginning-search-backward
+[[ -n "${key[C-Down]}" ]]    && bindkey "${key[C-Down]}"    history-beginning-search-forward
+[[ -n "${key[C-Left]}" ]]    && bindkey "${key[C-Left]}"    backward-word
+[[ -n "${key[C-Right]}" ]]   && bindkey "${key[C-Right]}"   forward-word
+[[ -n "${key[Alt-Left]}" ]]  && bindkey "${key[Alt-Left]}"  backward-word
+[[ -n "${key[Alt-Right]}" ]] && bindkey "${key[Alt-Right]}" forward-word
 
 # Pipe through $PAGER and execute
 bindkey -s '^P' "|$PAGER\n"
