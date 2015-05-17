aed472339ea1b5736e9d04b3a174d22e3e4dd648
Alejandro Avilés <omegak@gmail.com>
2015-05-17 20:56:09 +0200

    virtualenv: move checking venv to topic its folder

--- virtualenv/init.zsh
+++ virtualenv/init.zsh
@@ -2,3 +2,6 @@ export PROJECT_HOME=$PROJECTS
 export VIRTUALENVWRAPPER_HOOK_DIR=$ZSH/virtualenv/hooks
 
 source /usr/local/bin/virtualenvwrapper.sh
+
+# enable checking for virtualenv on cd
+workon_venv
