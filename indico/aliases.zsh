70a48d8d72bd8deb133fd66757daeb5f0ce97592
Alejandro Avilés <aavilesd@cern.ch>
2015-05-21 10:13:33 +0200

    indico: add indico run alias

--- indico/aliases.zsh
+++ indico/aliases.zsh
@@ -1,1 +1,2 @@
-alias ir="indico runserver --quiet --with-ssl --ssl-key ~/host.key --ssl-cert ~/host.cert --enable-evalex --evalex-from `myip`"
+# Run indico with SSL (asumes ssl cert present in home folder)
+alias ndc="indico runserver --quiet --with-ssl --ssl-key ~/host.key --ssl-cert ~/host.cert --enable-evalex --evalex-from `myip`"
