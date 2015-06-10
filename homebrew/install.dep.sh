eb19e2a207e1a1211e53c4321983e7debc3ef106
Alejandro Avilés <aavilesd@cern.ch>
2015-06-10 10:46:24 +0200

    homebrew: Now with 95% more linux!

--- homebrew/install.dep.sh
+++ homebrew/install.dep.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-if [[ "$(uname -s)" != "Darwin" ]]; then
+if [[ "$(uname -s)" == "Darwin" ]]; then
+  repo='install/master/install'
+elif [[ "$(uname -s)" == "Linux" ]]; then
+  repo='linuxbrew/go/install'
+else
   pprint warning "Unsupported OS"
   exit 0
 fi
 
 # Check for Homebrew
 if test ! $(which brew); then
-  pprint step "Installing Homebrew for you"
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null > /tmp/$DOT_TOPIC_LOGFILE_SUFFIX 2>&1
+  pprint step "Installing Homebrew"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/${repo})" < /dev/null > /tmp/$DOT_TOPIC_LOGFILE_SUFFIX 2>&1
   if [[ $? != 0 ]]; then
     pprint error "Failed to install Homebrew"
     exit 1
---
+++ homebrew/path.zsh
@@ -0,0 +1,3 @@
+if [[ "$(uname -s)" == "Linux" ]]; then
+  export LD_LIBRARY_PATH="$HOME/.linuxbrew/lib:$LD_LIBRARY_PATH"
+fi
