a11aefa23c3918db363f857e5a97aca4865c16ac
Alejandro Avilés <omegak@gmail.com>
2015-06-21 12:30:33 +0200

    dot: make first install more readable

--- iterm/install.sh
+++ iterm/install.sh
@@ -5,7 +5,7 @@ if [[ "$(uname -s)" != "Darwin" ]]; then
   exit 0
 fi
 
-if [[ ! "$(whichapp iterm)" ]]; then
+if [[ ! "$(whichapp iterm 2> /dev/null)" ]]; then
   pprint warning "iTerm doesn't seem to be installed"
   exit 0
 fi
@@ -14,11 +14,11 @@ plist_name="com.googlecode.iterm2.plist"
 plist_source="$DOT/iterm/${plist_name}"
 plist_target="$HOME/Library/Preferences/${plist_name}"
 
-# Check if preferences were evere imported
+# Check if preferences were ever imported
 if [[ ! -f ${plist_target}.backup ]]; then
   if [[ $TERM_PROGRAM = 'iTerm.app' ]]; then
     pprint warning "iTerm can't overwrite its own preferences"
-    pprint warning "Run \`$ make install\` from Terminal instead"
+    pprint warning "Run \`$ dot install\` from Terminal instead"
     exit 1
   fi
   pprint step "Backing up prefs and importing yours"
