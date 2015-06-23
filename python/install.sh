0f12e0d0d05141492034f7e04cc06eac7a2d2716
Alejandro Avilés <omegak@gmail.com>
2015-06-24 00:33:34 +0200

    python: don't upgrade packages

--- python/install.sh
+++ python/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-syspip install --upgrade pip ipython tmuxp wdb.server > /tmp/$DOT_TOPIC_LOGFILE_SUFFIX 2>&1
+syspip install pip ipython tmuxp wdb.server > /tmp/$DOT_TOPIC_LOGFILE_SUFFIX 2>&1
 if [[ $? != 0 ]]; then
   pprint info-error "Failed to install some packages"
   exit 1
