fe4f505a39c2c2cdacdcfc86f3958dd96834dedb
Alejandro Avilés <aavilesd@cern.ch>
2015-06-23 10:50:55 +0200

    homebrew: fix linuxbrew paths

--- homebrew/path.zsh
+++
@@ -1,3 +0,0 @@
-if [[ "$(uname -s)" == "Linux" ]]; then
-  export LD_LIBRARY_PATH="$HOME/.linuxbrew/lib:$LD_LIBRARY_PATH"
-fi
--- system/path.zsh
+++ system/path.zsh
@@ -1,5 +1,11 @@
 export PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:$MANPATH"
 
+if [[ "$(uname -s)" == "Linux" ]]; then
+  export PATH="$HOME/.linuxbrew/bin:$PATH"
+  export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
+fi
+
+# Keep path clean from duplicates
 typeset -U PATH
 typeset -U MANPATH
