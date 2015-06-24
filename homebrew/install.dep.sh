f25a1a1cf647786b817387af01acc31759b23284
Alejandro Avilés <omegak@gmail.com>
2015-06-25 00:33:03 +0200

    Inline ifs

--- homebrew/install.dep.sh
+++ homebrew/install.dep.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 
-if [[ "$(uname -s)" == "Darwin" ]]; then
-  repo='install/master/install'
-elif [[ "$(uname -s)" == "Linux" ]]; then
-  repo='linuxbrew/go/install'
-else
-  pprint info-warn "Unsupported OS"
-  exit 0
-fi
+repo='install/master/install'
+[[ "$(uname -s)" == "Linux" ]] && repo='linuxbrew/go/install'
 
 # Check for Homebrew
 if test ! $(which brew); then
   pprint info-go "Installing Homebrew"
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/${repo})" < /dev/null > /tmp/$DOT_TOPIC_LOGFILE_SUFFIX 2>&1
-  if [[ $? != 0 ]]; then
-    pprint info-error "Failed to install Homebrew"
-    exit 1
-  fi
+  [[ $? != 0 ]] && pprint info-error "Failed to install Homebrew" && exit 1
 fi
 
 pprint info-ok "Homebrew is ready to brew -- Choo, choo!"
