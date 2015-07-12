5e66af7ce155c99c2089bad516b17b160e16afc1
Alejandro Avilés <omegak@gmail.com>
2015-07-12 12:43:16 +0200

    virtualenv: fix init.zsh for Linux

--- virtualenv/init.zsh
+++ virtualenv/init.zsh
@@ -1,7 +1,7 @@
 export PROJECT_HOME=$PROJECTS
 export VIRTUALENVWRAPPER_HOOK_DIR=$ZSH/virtualenv/hooks
 
-source /usr/local/bin/virtualenvwrapper.sh
+source virtualenvwrapper.sh
 
 # enable checking for virtualenv on cd
 workon_venv
