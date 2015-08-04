bf4ca71bbda6ecfbe69f7ecc6c967a629c0d47fc
Alejandro Avilés <omegak@gmail.com>
2015-08-05 00:38:28 +0200

    system: move Linux dependencies from zeroday

---
+++ system/install.dep.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+source $DOT/functions/try
+
+if [[ "$(uname -s)" == "Linux" ]]; then
+  source $DOT/functions/keepsudo
+
+  if [[ $(sudo -n true 2>&1) ]]; then
+    pprint info-warn "Can I has sudo? "
+    keepsudo
+  fi
+
+  pinstall apt build-essential
+  pinstall apt ruby
+  pinstall apt zsh
+
+  # Needed for python to compile with sugar
+  pinstall apt libbz2-dev
+  pinstall apt libreadline-dev
+  pinstall apt libsqlite3-dev
+
+  # Needed for brew-git
+  pinstall apt libx11-dev
+  pinstall apt libcurl4-gnutls-dev
+
+  # Needed for brew-ruby-build
+  pinstall apt m4
+fi
+
+exit $TRY_CODE
