bcba370fde054608572b73a89f3a34afb61c8ccc
Alejandro Avilés <omegak@gmail.com>
2015-11-25 00:26:23 +0100

    zsh: add brewed zsh to valid login shells

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,4 +2,10 @@
 source $DOT/.dot/functions/try
 
 try pinstall brew zsh
+
+if [[ ! "$(zgrep "`which zsh`" /etc/shells)" ]]; then
+  sudoplz -r "For adding zsh to valid shells"
+  which zsh | sudo tee -a /etc/shells > /dev/null
+fi
+
 exit $TRY_CODE
