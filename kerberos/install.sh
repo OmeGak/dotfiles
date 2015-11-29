4fc410bbc816e0da519e77e032d3b2400394b7f8
Alejandro Avilés <omegak@gmail.com>
2015-11-29 21:18:28 +0100

    kerberos: install in mac and remove openafs

--- kerberos/install.sh
+++ kerberos/install.sh
@@ -2,12 +2,10 @@
 source $DOT/.dot/functions/try
 
 if [[ $(uname -s) == 'Linux' ]]; then
-  try pinstall apt openafs-client
-  try pinstall apt openafs-module-dkms
-  try pinstall apt openafs-krb5
   try pinstall apt krb5-user
-  try pinstall apt krb5-config
   try pinstall apt kstart
+else
+  try pinstall brew kstart
 fi
 
 exit $TRY_CODE
