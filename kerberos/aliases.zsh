51583b5d32e9b12f2d9a3ba430d617c04347c581
Alejandro Avilés <omegak@gmail.com>
2015-11-29 21:19:49 +0100

    kerberos: more aliases

--- kerberos/aliases.zsh
+++ kerberos/aliases.zsh
@@ -1,1 +1,17 @@
-alias kstart="k5start -b -f ~/.keytab -K 10 -l 7d -r CERN.CH -u aavilesd -t"
+alias ktinit="kinit -kt $HOME/.keytab"
+alias ktstart="k5start -f $HOME/.keytab -U -b -K 10"
+
+if [[ $(uname -s) == 'Darwin' ]]; then
+  alias ktutil="ktutil -k $HOME/.keytab"
+
+  # Add a keytab for a principal
+  ktadd() {
+    local principal
+    echo -n "Principal: "
+    read principal
+    echo -n "Password: "
+    read -s password
+    ktutil add -p ${principal} --password=${password} -e aes256-cts-hmac-sha1-96 -V 1
+    ktutil add -p ${principal} --password=${password} -e arcfour-hmac-md5 -V 1
+  }
+fi
