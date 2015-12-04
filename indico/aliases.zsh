fa2399c087474be4f526e1409d51553f301db8d6
Alejandro Avilés <omegak@gmail.com>
2015-12-04 16:47:49 +0100

    indico: add ndc-dblog alias

--- indico/aliases.zsh
+++ indico/aliases.zsh
@@ -3,3 +3,9 @@ alias ndc="WERKZEUG_DEBUG_PIN=off indico runserver --quiet --keep-base-url --hos
 
 # Initializes required services
 alias ndc-init="pg_ctl start; zdaemon -C "$ZODB_CONFIG" start; sudo =nginx -c "$NGINX_CONF"; redis-server&; maildump_ctl start"
+
+# Launch dblog
+ndc-dblog() {
+  local args="-i indico/web/assets/blueprint.py -i indico/web/flask/session.py:62 -i 5:indico_ravem/plugin.py -i 5:indico_ravem/util.py -i 4:indico/modules/users/models/settings.py:100 -I /assets/js-vars/user.js"
+  echo -e "\033]0;INDICO DB LOG\007\c" && python $PROJECTS/indico/bin/utils/db_log.py -t 5 $=args $*
+}
