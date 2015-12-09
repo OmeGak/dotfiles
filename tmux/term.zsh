363466324209478fc13cdddfa678884320df86d0
Alejandro Avilés <omegak@gmail.com>
2015-12-09 18:08:06 +0100

    tmux: choose correct terminal in linux

--- tmux/term.zsh
+++ tmux/term.zsh
@@ -1,15 +1,20 @@
+TERMINFO_DIR="/usr/share/terminfo"
+[[ "$(uname -s)" == "Linux" ]] && TERMINFO_DIR="/lib/terminfo"
+
 # Fixes colors in tmux
 if [[ -z $TMUX ]]; then
     # may be xterm-256 depending on your distro
-    if [ -e /usr/share/terminfo/x/xterm+256color ]; then
+    if [ -e $TERMINFO_DIR/x/xterm+256color ]; then
         export TERM='xterm-256color'
     else
         export TERM='xterm'
     fi
 else
-    if [ -e /usr/share/terminfo/s/screen-256color ]; then
+    if [ -e $TERMINFO_DIR/s/screen-256color ]; then
         export TERM='screen-256color'
     else
         export TERM='screen'
     fi
 fi
+
+unset TERMINFO_DIR
