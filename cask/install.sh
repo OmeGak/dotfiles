bd0f358e261a61cfbdafa2e5d1ccbf5d41288d8d
Alejandro Avilés <omegak@gmail.com>
2016-03-28 17:50:58 +0200

    cask: cask now comes built-in with brew

--- cask/install.sh
+++ cask/install.sh
@@ -5,16 +5,6 @@ if [[ "$(uname -s)" != "Darwin" ]]; then
   exit 0
 fi
 
-# Check for Cask
-if [[ ! $(which brew-cask) ]]; then
-  pprint info-go "Installing brew-cask"
-  brew install caskroom/cask/brew-cask > /tmp/$DOT_TOPIC_LOGFILE_SUFFIX 2>&1
-  if [[ $? != 0 ]]; then
-    pprint info-error "Failed to install brew-cask"
-    exit 1
-  fi
-fi
-
 # Check if caskroom/versions is tapped
 if [[ -z $(brew tap | grep caskroom/versions) ]]; then
   pprint info-go "Tapping caskroom/versions formulas"
