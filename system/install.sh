771a67d6cf36b1bb6d8016bf1cee31d331a35b74
Alejandro Avilés <omegak@gmail.com>
2016-03-28 20:26:20 +0200

    system: install fonts if there are missing

--- system/install.sh
+++ system/install.sh
@@ -9,18 +9,34 @@ if [[ "$(uname -s)" == "Linux" ]]; then
   try pinstall apt xclip
 fi
 
+
+# -- Install missing fonts ----------------------------------------------------
+
 fonts_dir="$HOME/Library/Fonts"
 if [[ "$(uname -s)" == "Linux" ]]; then
   fonts_dir="$HOME/.fonts"
   refresh='pprint info-go "Refreshing fonts"; fc-cache -f 2>&1'
 fi
 
-fonts_installed=${fonts_dir}/.dot
-if [[ ! -f "${fonts_installed}" ]]; then
-  pprint info-go "Installing fonts into ${fonts_dir}"
+target() {
+  echo "${fonts_dir}/$(basename "$1")"
+}
+
+font_files=("$DOT/system/fonts/*")
+missing_fonts=()
+for font_src in ${font_files}; do
+  if [[ ! -e "$(target "${font_src}")" ]]; then
+    missing_fonts+=("${font_src}")
+  fi
+done
+
+if [[ -n ${missing_fonts} ]]; then
+  pprint info-go "Installing missing fonts into ${fonts_dir}"
   mkdir -p "${fonts_dir}"
-  cp $DOT/system/fonts/* "${fonts_dir}"
-  touch "${fonts_installed}"
+  for font_src in ${missing_fonts[@]}; do
+    # Symlinking fonts is not supported in OSX
+    cp "${font_src}" "$(target "${font_src}")"
+  done
   sh -c "${refresh}"
 fi
 
