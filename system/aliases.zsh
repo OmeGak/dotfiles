f6303cfc673407cfdc5c1e94621fa36dfe8529d9
Alejandro Avilés <alejandro.aviles@cern.ch>
2016-06-22 14:06:01 +0200

    system: snippet for downloading whole websites

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,21 +1,13 @@
-# grc overides for ls
-#   Made possible through contributions from generous benefactors like
-#   `brew install coreutils`
-if $(gls &>/dev/null)
-then
-    LS="gls"
-else
-    LS="ls"
-fi
-
 # places
 alias ..="cd .."
 alias code="cd $HOME/Code"
 
 # ls
+[[ $(which gls) ]] && LS="gls" || LS="ls"
 alias ls="$LS -F --color"
 alias ll="$LS -lh --group-directories-first --color"
 alias lla="$LS -lhA --group-directories-first --color"
+unset LS
 
 # path
 alias path='echo $PATH | tr : "\n"'
@@ -31,4 +23,11 @@ if [[ "$(uname -s)" == "Darwin" ]]; then
   alias tac='tail -r'
 fi
 
-unset LS
+
+# -- Snippets -----------------------------------------------------------------
+
+# Download website for full offline navigation
+# Source: http://www.kossboss.com/linux---wget-full-website
+if [[ $(which wget) ]]; then
+  alias wget-full="wget --limit-rate=200k --no-clobber --convert-links --random-wait -r -p -E -e robots=off -U mozilla -N"
+fi
