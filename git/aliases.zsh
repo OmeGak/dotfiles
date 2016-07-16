7c3d67d8ae0901c465c1bb89d956bee899a860f8
Alejandro Avilés <omegak@gmail.com>
2016-07-16 16:27:07 +0200

    git: cleanup aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,21 +1,11 @@
 # Use `hub` as our git wrapper:
 #   http://defunkt.github.com/hub/
 hub_path=$(which hub)
-if (( $+commands[hub] ))
-then
-  alias git=$hub_path
+if (( $+commands[hub] )); then
+  alias git=${hub_path}
 fi
 
-# The rest of my fun git aliases
-alias gl='git pull --prune'
-alias glr='git pull --rebase --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
-alias gd='git diff'
-alias gc='git commit'
-alias gca='git commit -a'
-alias gco='git checkout'
-alias gcb='git copy-branch-name'
-alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias gl="git lg"
+alias gc="git commit"
+alias gd="git diff"
+alias gs="git status"
