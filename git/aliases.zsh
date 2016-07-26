037e6879dbb6a56b5a4825ff1af4428017041a1b
Alejandro Avilés <omegak@gmail.com>
2016-07-26 22:13:01 +0200

    git: update hub alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,8 +1,6 @@
-# Use `hub` as our git wrapper:
-#   http://defunkt.github.com/hub/
-hub_path=$(which hub)
-if (( $+commands[hub] )); then
-  alias git=${hub_path}
+# Use `hub` as git wrapper
+if exists hub; then
+  eval "$(hub alias -s)"
 fi
 
 alias gl="git lg"
