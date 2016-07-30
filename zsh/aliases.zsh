98a7075870ffc792db908397d6a3a076fe59a975
Alejandro Avilés <omegak@gmail.com>
2016-07-30 09:49:14 +0200

    zsh: add histrl function

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,9 +1,6 @@
 alias cddot='cd $DOT'
 alias reload!='. ~/.zshrc'
 
-# Refresh history from file
-alias histr='fc -RI'
-
 
 # -- Directory stack navigation -----------------------------------------------
 
