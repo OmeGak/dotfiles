4bcd2dc3d4bc34625a7c3285834879e49e6d674a
Alejandro Avilés <omegak@gmail.com>
2016-11-28 23:37:09 +0100

    atom: enter sass-lint

--- atom/install.sh
+++ atom/install.sh
@@ -5,4 +5,5 @@ try pinstall npm eslint
 try pinstall npm eslint-plugin-promise
 try pinstall npm eslint-config-standard
 try pinstall npm eslint-plugin-standard
+try pinstall npm sass-lint
 exit $TRY_CODE
