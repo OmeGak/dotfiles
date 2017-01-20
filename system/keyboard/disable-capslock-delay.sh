ad8409dc96b095b5a6e6ede1fe8e98f14b257082
Alejandro Avilés <alejandro.aviles@cern.ch>
2017-01-20 11:23:13 +0100

    system: disable capslock delay on startup

--- system/keyboard/disable-capslock-delay.sh
+++ system/keyboard/disable-capslock-delay.sh
@@ -1,10 +1,17 @@
+#!/usr/bin/env bash
 # Source: http://apple.stackexchange.com/a/199958/118122
 
 # First compile
 # gcc -o disable-capslock-delay disable-capslock-delay.c
 
+# To run on startup add next line to the root crontab (sudo crontab -e)
+# @reboot /home/<user>/.dotfiles/system/keyboard/disable-capslock-delay.sh
+
+# This script needs to run as sudo, so it cannot use $DOT
+SCRIPTPATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # Find the right hidraw device
 HIDDEVICE=$(dmesg | grep Apple | grep Keyboard | grep input0 | tail -1 | sed -e 's/.*hidraw\([[:digit:]]\+\).*/\/dev\/hidraw\1/')
 
 # Send a "Report Feature" to the device
-$DOT/system/keyboard/disable-capslock-delay $HIDDEVICE
+$SCRIPTPATH/disable-capslock-delay $HIDDEVICE
