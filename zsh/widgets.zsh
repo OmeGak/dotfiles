c6b977c29d679ad9d139942dc94672a720d46831
Alejandro Avilés <omegak@gmail.com>
2017-03-28 10:43:01 +0200

    zsh: fix widgets in zsh 5.3

--- zsh/widgets.zsh
+++ zsh/widgets.zsh
@@ -4,7 +4,7 @@
 # -- Debugging ----------------------------------------------------------------
 
 # Source: http://stackoverflow.com/a/2649659
-show-buffers() {
+zle-show-buffers() {
   local nl=$'\n' kr
   typeset -T kr KR $'\n'
   KR=($killring)
@@ -18,39 +18,35 @@ show-buffers() {
   buffers+="Killring:$nl$nl$kr"
   zle -M "$buffers"
 }
-zle -N show-buffers
-unset show-buffers
+zle -N show-buffers zle-show-buffers
 
 
 # -- Movement -----------------------------------------------------------------
 
-backward-word-end() {
+zle-backward-word-end() {
   [[ $RBUFFER[1] == ' ' ]] && zle backward-char
   zle vi-backward-blank-word-end
   [[ $#LBUFFER != 0 ]] && zle forward-char
 }
-zle -N backward-word-end
-unset backward-word-end
+zle -N backward-word-end zle-backward-word-end
 
-forward-word-end() {
+zle-forward-word-end() {
   zle vi-forward-blank-word-end
   zle forward-char
 }
-zle -N forward-word-end
-unset forward-word-end
+zle -N forward-word-end zle-forward-word-end
 
 
 # -- Completion ---------------------------------------------------------------
 
 # Show dots while waiting to complete. Useful for systems with slow net access,
 # like those places where they use giant, slow NFS solutions. (Hint.)
-expand-or-complete-with-dots() {
+zle-expand-or-complete-with-dots() {
   echo -n "\e[31m...\e[0m"
   zle expand-or-complete
   zle redisplay
 }
-zle -N expand-or-complete-with-dots
-unset expand-or-complete-with-dots
+zle -N expand-or-complete-with-dots zle-expand-or-complete-with-dots
 
 
 # -- History control ----------------------------------------------------------
@@ -62,7 +58,7 @@ autoload -U down-line-or-beginning-search
 zle -N down-line-or-beginning-search
 
 # Remove input/suggestion from history
-forget-history() {
+zle-forget-history() {
   local cmd=$BUFFER$POSTDISPLAY
   # Trim trailing whitespace
   cmd=${cmd//[[:space:]]%/}
@@ -74,8 +70,7 @@ forget-history() {
   histrm "${cmd}"
   histrl
 }
-zle -N forget-history
-unset forget-history
+zle -N forget-history zle-forget-history
 
 
 # -- Misc ---------------------------------------------------------------------
@@ -86,21 +81,19 @@ zle -N self-insert url-quote-magic
 
 # Enter white spaces around the inserted key
 # Based on: http://www.zsh.org/mla/users/2006/msg00690.html
-self-insert-padded() {
+zle-self-insert-padded() {
   setopt localoptions noksharrays
   zle self-insert
   [[ $LBUFFER[-2] != " " ]] && LBUFFER[-1]=" $LBUFFER[-1]"
   LBUFFER[-1]="$LBUFFER[-1] "
 }
-zle -N self-insert-padded
-unset self-insert-padded
+zle -N self-insert-padded zle-self-insert-padded
 
 
 # -- Plugins ------------------------------------------------------------------
 
-next-suggested-word() {
+zle-next-suggested-word() {
   [[ $RBUFFER[1] == ' ' ]] && zle .forward-word
   zle .forward-word
 }
-zle -N next-suggested-word
-unset next-suggested-word
+zle -N next-suggested-word zle-next-suggested-word
