c01237cffbd91991fa14713e3900038dbdb7dbd6
Alejandro Avilés <omegak@gmail.com>
2017-05-21 16:10:36 +0200

    dot: reorganize public core bin

--- .dot/zsh/path.zsh
+++ .dot/zsh/path.zsh
@@ -1,10 +1,9 @@
-# export PATH="$DOT/bin:$DOT/.dot/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:$PATH"
-export PATH="$DOT/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:$PATH"
+export PATH="$DOT/bin:$DOT/.dot/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:$PATH"
 export MANPATH="/usr/local/man:$MANPATH"
 
 if [[ "$OS" == "Linux" ]]; then
-  export PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
-  export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
+  PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
+  MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
 fi
 
 # Keep path clean from duplicates
