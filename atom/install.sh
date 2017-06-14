c23130df7dfa5f4f77d1a219edf6775dbb0fa9a2
Alejandro Avilés <omegak@gmail.com>
2017-06-14 11:13:14 +0200

    atom: install flake8

--- atom/install.sh
+++ atom/install.sh
@@ -9,12 +9,13 @@ try pinstall brew ctags
 
 # Linters
 try pinstall brew shellcheck
+try pinstall gem rubocop
 try pinstall npm dockerlint
 try pinstall npm eslint
 try pinstall npm eslint-plugin-promise
 try pinstall npm eslint-config-standard
 try pinstall npm eslint-plugin-standard
 try pinstall npm sass-lint
-try pinstall gem rubocop
+try pinstall pip flake8
 
 exit $TRY_CODE
