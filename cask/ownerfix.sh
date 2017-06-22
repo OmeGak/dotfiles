b06b7310e95b7c2b2ee757b48e8ddb55d403248d
Alejandro Avilés <omegak@gmail.com>
2017-06-22 02:03:58 +0200

    cask: remove old fix

--- cask/ownerfix.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# osxfuse installer can fuck up the owner of pkgconfig
-pkgconfig_owner=`ls -ld /usr/local/lib/pkgconfig | awk '{print $3}'`
-if [[ $pkgconfig_owner != $(whoami) ]]; then
-  echo "Re-establishing owner of /usr/local/lib/pkgconfig"
-  sudo chown $(whoami) /usr/local/lib/pkgconfig
-  sudo chgrp admin /usr/local/lib/pkgconfig
-fi
