03c9e458d9784b0bb16aeaeeb68e8544b9d38f6a
Alejandro Avilés <omegak@gmail.com>
2017-06-27 18:25:30 +0200

    ansible: add dnsmasq

--- ansible/install.sh
+++ ansible/install.sh
@@ -2,4 +2,13 @@
 source $DOT/.dot/functions/try
 
 try pinstall brew ansible
+try pinstall brew dnsmasq
+
+dnsmasqconf_source="$DOT/ansible/dnsmasq.conf"
+dnsmasqconf_target="/usr/local/etc/dnsmasq.conf"
+if [[ ! $(checklink "${dnsmasqconf_source}" "${dnsmasqconf_target}") ]]; then
+  pprint info-go "Installing dnsmasq configuration"
+  mksymlink "${dnsmasqconf_source}" "${dnsmasqconf_target}"
+fi
+
 exit $TRY_CODE
