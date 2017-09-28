70758f034543233bd8f0b6f77f5105626f1128f9
Alejandro Avilés <omegak@gmail.com>
2017-09-28 15:23:29 +0200

    crypto: add crypto topic

---
+++ crypto/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+source $DOT/.dot/functions/try
+
+try pinstall brew bitcoin
+
+bitcoin_conf="bitcoin.conf"
+bitcoin_conf_source="$DOT/crypto/${bitcoin_conf}"
+bitcoin_conf_target="$HOME/Library/Application Support/Bitcoin/${bitcoin_conf}"
+
+# Link bitcoin.conf
+if [[ ! $(checklink "${bitcoin_conf_target}" "${bitcoin_conf_source}") ]]; then
+  pprint info-go "Installing bitcoin.conf"
+  mksymlink "${bitcoin_conf_source}" "${bitcoin_conf_target}"
+fi
+
+exit $TRY_CODE
