1fd4851407186e2eaa829a97ef4f54cc89af3ed0
Alejandro Avilés <omegak@gmail.com>
2017-10-06 21:40:37 +0200

    docker: install completions correctly

--- docker/install.sh
+++ docker/install.sh
@@ -7,4 +7,20 @@ if [[ $OS == 'Linux' ]]; then
 fi
 
 try pinstall cask docker
+
+# XXX: Workaround until the completion files are correctly named upstream
+# https://github.com/docker/for-mac/issues/1948
+completions_dir="/Applications/Docker.app/Contents/Resources/etc"
+completions=("docker" "docker-compose" "docker-machine")
+for completion in ${completions[*]}; do
+  completion_source="${completions_dir}/${completion}.zsh-completion"
+  completion_target="${completions_dir}/_${completion}"
+  if [[ ! $(checklink "${completion_source}" "${completion_target}") ]]; then
+    pprint info-go "Installing completions for ${completion}"
+    # XXX: zsh seems not to load completion files if they are symlinks here
+    mv "${completion_source}" "${completion_target}"
+    mksymlink "${completion_target}" "${completion_source}"
+  fi
+done
+
 exit $TRY_CODE
