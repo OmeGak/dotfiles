cb6ceac611be9e0d31b5e936e4a81bbfae644f72
Alejandro Avilés <omegak@gmail.com>
2018-03-20 18:50:24 +0100

    python: bump python tools

--- python/install.sh
+++ python/install.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 source $DOT/.dot/functions/try
 
+# Python 2 tools
 try pinstall pip2 pip
-try pinstall pip3 pipenv
-try pinstall pip2 ipython
 try pinstall pip2 virtualenv
 try pinstall pip2 virtualenvwrapper
-try pinstall pip2 wdb.server
+
+# Python 3 tools
+try pinstall pip3 pip
+try pinstall pip3 ipython
+try pinstall pip3 pipenv
+try pinstall pip3 tox
+try pinstall pip3 wdb.server
+
 exit $TRY_CODE
