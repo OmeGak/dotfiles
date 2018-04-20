7d42063aeed4726a7da121041ab7db9cb91d22d8
Alejandro Avilés <omegak@gmail.com>
2018-04-20 21:42:10 +0200

    python: clean up install script

--- python/install.sh
+++ python/install.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 source $DOT/.dot/functions/try
 
-# Python 2 tools
-try pinstall pip2 pip
-
-# Python 3 tools
-try pinstall pip3 pip
+try pinstall brew pipenv
+try pinstall brew tox
 try pinstall pip3 ipython
-try pinstall pip3 pipenv
-try pinstall pip3 tox
 try pinstall pip3 wdb.server
 
 exit $TRY_CODE
