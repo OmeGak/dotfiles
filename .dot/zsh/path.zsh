ea955330228283d3e8da459fb96ad0a2c13a3431
Alejandro Avilés <omegak@gmail.com>
2018-04-20 18:24:25 +0200

    dot: improve path building

--- .dot/zsh/path.zsh
+++ .dot/zsh/path.zsh
@@ -1,11 +1,28 @@
-export PATH="$DOT/bin:$DOT/.dot/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:$PATH"
-export MANPATH="/usr/local/man:$MANPATH"
+buildpath() {
+  local localpath="$HOME/.local/bin"
+  local dotpath="$DOT/bin:$DOT/.dot/bin"
+  local binpath="${dotpath}:${localpath}:$BREWDIR/bin:/usr/bin:/bin"
+  local sbinpath="$BREWDIR/sbin:/usr/sbin:/sbin"
+  echo "${binpath}:${sbinpath}"
+}
 
-if [[ "$OS" == "Linux" ]]; then
-  PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
-  MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
+buildmanpath() {
+  [[ "$OS" == 'Darwin' ]] && echo "$BREWDIR/man" \
+                          || echo "$BREWDIR/share/man"
+}
+
+# Explicitly run macOS path builder with clean paths, otherwise it scrambles it
+if [ -x /usr/libexec/path_helper ]; then
+  PATH="" MANPATH="" eval $(/usr/libexec/path_helper -s)
 fi
+
+export PATH="$(buildpath):$PATH"
+export MANPATH="$(buildmanpath):$MANPATH"
 
 # Keep path clean from duplicates
 typeset -U PATH
 typeset -U MANPATH
+
+# Clean up
+unset -f buildpath
+unset -f buildmanpath
