7737b509f63794539264fd2cbee6573dd9de1794
Alejandro Avilés <omegak@bity.com>
2019-02-15 10:42:29 +0100

    git: install git-lfs

--- git/install.sh
+++ git/install.sh
@@ -10,6 +10,7 @@ try brew tap git-duet/tap > /dev/null 2>&1
 try pinstall brew git ${args}
 try pinstall brew hub
 try pinstall brew git-duet
+try pinstall brew git-lfs
 try pinstall npm git-standup
 
 # Required for git-ngrok
