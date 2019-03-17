72111cab89c05bf1d6ba3265ba220b9d0fac6e82
Alejandro Avilés <omegak@gmail.com>
2019-03-17 15:34:37 +0100

    brew: fix apps.sh install script

--- brew/apps.sh
+++ brew/apps.sh
@@ -6,16 +6,15 @@ if [[ "$OS" != "Darwin" ]]; then
 fi
 
 # Get list of new casks
-brew cask ls -1 > /tmp/cask-ls
-new_casks=$(cat $DOT/cask/casks.txt | grep -v \# | grep -vwf /tmp/cask-ls)
+brew cask list > /tmp/cask-list
+new_casks=$(cat $DOT/brew/casks.txt | grep -v \# | grep -vwf /tmp/cask-list)
 
 # Install new casks
 if [[ ! -z "${new_casks}" ]]; then
-  brew cask install ${new_casks} --appdir=/Applications
+  brew cask install ${new_casks}
 fi
 
 # Cleanup
-$DOT/cask/ownerfix.sh
 brew cask cleanup
 
 exit 0
