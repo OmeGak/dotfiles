1e10ca0d6a77560e972bd0070827860364f9bd62
Alejandro Avilés <omegak@gmail.com>
2019-03-18 20:26:52 +0100

    indico: remove unused topic

--- indico/env.zsh
+++
@@ -1,6 +0,0 @@
-export INDICO_DBDIR=/usr/local/var/postgres
-export ZODB_CONF=$PROJECTS/indico/etc/zdctl.conf
-export NGINX_CONF=$DOT/indico/nginx.conf
-
-# Default pg_ctl data dir
-export PGDATA=$INDICO_DBDIR
--- indico/install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# shellcheck source=../.dot/functions/try
-source $DOT/.dot/functions/try
-
-try pinstall brew cowsay
-exit $TRY_CODE
