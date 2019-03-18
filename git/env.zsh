1890b319ecd46d75d01d0c28b4ece9bf38c141d8
Alejandro Avilés <omegak@gmail.com>
2019-03-18 18:46:00 +0100

    git: configure user with env vars

--- git/env.zsh
+++ git/env.zsh
@@ -1,2 +1,9 @@
+# Git user setup with env vars so to allow overrides with `direnv`
+# Inspired by https://collectiveidea.com/blog/archives/2016/04/04/multiple-personalities-in-git
+export GIT_AUTHOR_NAME="Alejandro Avilés"
+export GIT_COMMITTER_NAME=$GIT_AUTHOR_NAME
+export GIT_AUTHOR_EMAIL="omegak@gmail.com"
+export GIT_COMMITTER_EMAIL=$GIT_AUTHOR_EMAIL
+
 export GIT_DUET_GLOBAL=true
 export GIT_DUET_ROTATE_AUTHOR=true
