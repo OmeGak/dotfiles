7e9c5427edec40f1cd93782a4e5ea96e564a3c9c
Alejandro Avilés <omegak@gmail.com>
2019-08-26 15:16:52 +0200

    python: install ipython from Homebrew

--- python/install.sh
+++ python/install.sh
@@ -2,9 +2,9 @@
 # shellcheck source=../.dot/functions/try
 source $DOT/.dot/functions/try
 
+try pinstall brew ipython
 try pinstall brew pipenv
 try pinstall brew tox
-try pinstall pip3 ipython
 try pinstall pip3 wdb.server
 
 exit $TRY_CODE
