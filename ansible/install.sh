7f707acbf868b9b14ba07fb1eff5d7acbfcf2b46
Alejandro Avilés <omegak@gmail.com>
2019-11-05 16:21:00 +0100

    ansible: fix install script

--- ansible/install.sh
+++ ansible/install.sh
@@ -7,7 +7,7 @@ try pinstall brew dnsmasq
 
 dnsmasqconf_source="$DOT/ansible/dnsmasq.conf"
 dnsmasqconf_target="/usr/local/etc/dnsmasq.conf"
-if [[ ! $(checklink "${dnsmasqconf_source}" "${dnsmasqconf_target}") ]]; then
+if [[ ! $(checklink "${dnsmasqconf_target}" "${dnsmasqconf_source}") ]]; then
   pprint info-go "Installing dnsmasq configuration"
   mksymlink "${dnsmasqconf_source}" "${dnsmasqconf_target}"
 fi
