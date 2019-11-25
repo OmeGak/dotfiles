7b3869f53a199b9ccbce838be7ee60f367c1dbea
Alejandro Avilés <omegak@gmail.com>
2019-11-25 16:36:13 +0100

    dev: move development config to dev topic

--- atom/env.zsh
+++ atom/env.zsh
@@ -1,4 +1,1 @@
 export ATOM_REPOS_HOME="${PROJECTS}/atom-dev"
-
-# Ignores shellcheck no-shebang errors
-export SHELLCHECK_OPTS="-e SC2148"
---
+++ dev/env.zsh
@@ -0,0 +1,2 @@
+# Ignores shellcheck no-shebang errors
+export SHELLCHECK_OPTS="-e SC2148"
