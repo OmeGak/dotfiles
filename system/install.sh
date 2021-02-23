6496426eec75ced975d3cbf0e8301d2ef3f04cf2
Alejandro Avilés <omegak@gmail.com>
2021-02-23 13:23:01 +0100

    system: install fonts via homebrew

--- system/install.sh
+++ system/install.sh
@@ -21,6 +21,10 @@ try pinstall brew youtube-dl
 try pinstall gem boom
 try pinstall pip3 xkcdpass
 
+# fonts
+try pinstall brew font-sauce-code-pro-nerd-font
+try pinstall brew font-source-code-pro
+
 if [[ "$OS" == "Linux" ]]; then
   try pinstall apt xclip
 fi
@@ -34,37 +38,6 @@ fi
 # -- Create expected directories -----------------------------------------------
 
 mkdir -p ${HOME}/Code
-
-
-# -- Install missing fonts ----------------------------------------------------
-
-fonts_dir="$HOME/Library/Fonts"
-if [[ "$OS" == "Linux" ]]; then
-  fonts_dir="$HOME/.fonts"
-  refresh='pprint info-go "Refreshing fonts"; fc-cache -f 2>&1'
-fi
-
-target() {
-  echo "${fonts_dir}/$(basename "$1")"
-}
-
-font_files=("$DOT/system/fonts/*")
-missing_fonts=()
-for font_src in ${font_files}; do
-  if [[ ! -e "$(target "${font_src}")" ]]; then
-    missing_fonts+=("${font_src}")
-  fi
-done
-
-if [[ -n ${missing_fonts} ]]; then
-  pprint info-go "Installing missing fonts into ${fonts_dir}"
-  mkdir -p "${fonts_dir}"
-  for font_src in ${missing_fonts[@]}; do
-    # Symlinking fonts is not supported in macOS
-    cp "${font_src}" "$(target "${font_src}")"
-  done
-  sh -c "${refresh}"
-fi
 
 
 # -- Create other symlinks -----------------------------------------------------
