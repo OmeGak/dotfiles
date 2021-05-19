324b1f06efdc436af3c84671c9c1f93945bfa78d
Alejandro Avilés <omegak@gmail.com>
2021-05-19 23:53:18 +0200

    dev: remove rubocop

--- dev/install.sh
+++ dev/install.sh
@@ -5,7 +5,6 @@ source $DOT/.dot/functions/try
 # Linters
 try pinstall brew hlint
 try pinstall brew shellcheck
-try pinstall gem rubocop
 try pinstall npm dockerlint
 try pinstall npm eslint
 try pinstall npm eslint-plugin-promise
