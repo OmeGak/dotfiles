4fca31ecaf29664794290e26cfa7f68ce5242bbe
Alejandro Avilés <omegak@gmail.com>
2021-06-10 16:53:00 +0200

    system: remove failing, unnecessary command

--- system/env.zsh
+++ system/env.zsh
@@ -11,4 +11,3 @@ export GREP_OPTIONS='--color=auto'
 # Set bat as the colorizing pager for man
 # source: https://github.com/sharkdp/bat#man
 export MANPAGER="sh -c 'col -bx | bat --language man --plain'"
-man 2 select
