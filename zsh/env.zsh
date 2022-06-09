b184edcd03a185fa873ac626d41c25f5896093ed
Alejandro Avilés <omegak@gmail.com>
2022-06-09 15:15:49 +0200

    zsh: move term config to env.zsh file

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,3 +1,22 @@
+# -- Configure TERM info -------------------------------------------------------
+
+TERMINFO_DIR="/usr/share/terminfo"
+[[ "$OS" == "Linux" ]] && TERMINFO_DIR="/lib/terminfo"
+
+# Always use screen on tmux
+[[ -z $TMUX ]] && TERM_TYPE='xterm' || TERM_TYPE='screen'
+
+# Apply 256 only if available
+if [ ! -z $TERMINFO_DIR/**/$TERM_TYPE-256color(N) ]; then
+  TERM_COLOR='-256color'
+fi
+
+export TERM="${TERM_TYPE}${TERM_COLOR}"
+unset TERMINFO_DIR TERM_TYPE TERM_COLOR
+
+
+# -- Configure FZF -------------------------------------------------------------
+
 export FZF_DEFAULT_COMMAND='fd .'
 export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND --type=directory"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
