6f0f70faae5127ecc370fe180baefc95bceafcc8
Alejandro Avilés <omegak@gmail.com>
2022-07-21 12:19:29 +0200

    docker: install completions from homebrew

--- docker/install.sh
+++ docker/install.sh
@@ -2,24 +2,6 @@
 # shellcheck source=../.dot/functions/try
 source $DOT/.dot/functions/try
 
-if [[ $OS == 'Linux' ]]; then
-  pprint info-warn "Step skipped: Better not install in Linux"
-  exit 0
-fi
-
-# XXX: Workaround until the completion files are correctly named upstream
-# https://github.com/docker/for-mac/issues/1948
-completions_dir="/Applications/Docker.app/Contents/Resources/etc"
-completions=("docker" "docker-compose")
-for completion in ${completions[*]}; do
-  completion_bundled="${completions_dir}/${completion}.zsh-completion"
-  completion_fixed="${completions_dir}/_${completion}"
-  if [[ ! $(checklink "${completion_bundled}" "${completion_fixed}") ]]; then
-    pprint info-go "Installing completions for ${completion}"
-    # XXX: zsh seems not to load completion files if they are symlinks here
-    mv "${completion_bundled}" "${completion_fixed}"
-    mksymlink "${completion_fixed}" "${completion_bundled}"
-  fi
-done
+try pinstall brew docker-completion
 
 exit $TRY_CODE
