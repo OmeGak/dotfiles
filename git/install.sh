962abcf1f4363d4b42ec70f482b84bcb5204320f
Alejandro Avilés <omegak@gmail.com>
2022-11-29 15:37:42 +0100

    git: clean up

--- git/install.sh
+++ git/install.sh
@@ -2,17 +2,10 @@
 # shellcheck source=../.dot/functions/try
 source $DOT/.dot/functions/try
 
-if [[ "$OS" == "Linux" ]]; then
-  args="--with-brewed-curl --with-brewed-openssl --without-tcl-tk"
-fi
-
-try pinstall brew git "${args}"
+try pinstall brew git
 try pinstall brew git-interactive-rebase-tool
 try pinstall brew git-lfs
 try pinstall brew hub
 try pinstall brew tig
-
-# Required for git-ngrok
-try pinstall brew jq
 
 exit $TRY_CODE
