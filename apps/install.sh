ffd6fa9198d25ccfb90589d6a15af1dbcb62463c
Alejandro Avilés <omegak@gmail.com>
2022-12-07 15:34:00 +0100

    apps: install spotdl

--- apps/install.sh
+++ apps/install.sh
@@ -12,6 +12,16 @@ try pinstall brew speedtest-cli
 try pinstall brew yt-dlp
 try pinstall gem boom
 try pinstall pip xkcdpass
-try pinstall pip ytmdl
+try pinstall pip spotdl
+
+spotdl_conf="spotdl.config.json"
+spotdl_conf_source="$DOT/apps/${spotdl_conf}"
+spotdl_conf_target="$HOME/.spotdl/${spotdl_conf}"
+
+# Link spotdl configuration
+if [[ ! $(checklink "${spotdl_conf_target}" "${spotdl_conf_source}") ]]; then
+  pprint info-go "Installing spotdl configuration"
+  mksymlink "${spotdl_conf_source}" "${spotdl_conf_target}"
+fi
 
 exit $TRY_CODE
