30c3657ba6db86b54772bd325f1a6384539b3b62
Alejandro Avilés <omegak@gmail.com>
2025-01-28 21:49:39 +0100

    apps: add alias for downloading audio with yt-dlp

--- apps/aliases.rc.zsh
+++ apps/aliases.rc.zsh
@@ -4,4 +4,5 @@ fi
 
 if (( $+commands[yt-dlp] )); then
   alias ytdl='yt-dlp --config-locations=~/.yt-dlp.conf'
+  alias ytdl-audio="ytdl --extract-audio --format=bestaudio --audio-format=mp3 --audio-quality=0 --paths=~/Downloads/ytdl/music/ --output='%(title)s [%(id)s].%(ext)s'"
 fi
--- apps/completion.zsh
+++ apps/completion.zsh
@@ -1,3 +1,4 @@
 if (( $+commands[yt-dlp] )); then
   compdef ytdl='yt-dlp'
+  compdef ytdl-audio='yt-dlp'
 fi
