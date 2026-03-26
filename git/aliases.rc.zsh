e6005f40414e0ae0a76d8d90e8cb4f2737ba0144
Alejandro Avilés <omegak@gmail.com>
2026-03-26 17:10:43 +0100

    git: add cdr alias to cd to root of git repo

--- git/aliases.rc.zsh
+++ git/aliases.rc.zsh
@@ -11,3 +11,12 @@ fi
 alias gc="git commit"
 alias gl="git lg"
 alias gs="git status"
+
+# Change directory to root of the Git repository
+cdr() {
+  if git rev-parse --is-inside-work-tree &>/dev/null; then
+    cd "$(git rev-parse --show-toplevel)"
+  else
+    echo "Not inside a git repository."
+  fi
+}
